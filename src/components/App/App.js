c5acbe4c6176fa74517e4fe4e8c462ce471fcdc4
Roman Didukh <newrdlink@yandex.ru>
2020-09-20 20:20:38 +0300

    начать добавлять компоненты

---
+++ src/components/App/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import logo from "../../logo.svg";
+import Header from "../Header/Header";
+import Footer from "../Footer/Footer";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="root">
+      <Header />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
